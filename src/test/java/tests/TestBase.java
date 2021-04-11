99f0fff1fdcdd29d3e4aa4cd2a052b472cf294ed
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-04-11 19:06:00 +0300

    Allur added

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import config.LaunchConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentHelper.*;
+
+public class TestBase {
+    static final LaunchConfig config = ConfigFactory.create(LaunchConfig.class, System.getProperties());
+    @BeforeAll
+    static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
+
+        if (config.webDriverUrl() != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = config.webDriverUrl();
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if(System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
+    }
+}
